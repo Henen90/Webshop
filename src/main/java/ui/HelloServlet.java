3fc308f6924cd1419e96fc12d034e4002775d5cd
Herman Olsson <herman.olsson.1996@gmail.com>
2025-10-01 13:30:24 +0200

    asdasd

--- src/main/java/ui/HelloServlet.java
+++ src/main/java/ui/HelloServlet.java
@@ -13,16 +13,7 @@ public class HelloServlet extends HttpServlet {
 
     public void init() {
         message = "Hello Worldis!";
-        try {
-            // Startar H2:s webbserver/konsol på port 8082
-            Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
-            System.out.println("H2 Console är startad på http://localhost:8082");
-            System.out.println("Tryck [Enter] för att avsluta.");
-            System.in.read();
 
-        } catch (SQLException | IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
